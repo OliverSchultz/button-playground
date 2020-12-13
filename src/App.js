706b5636e7e76d993e6e934ba1f3ba0b915d2f86
OliverSchultz <oliverschultz1979@gmail.com>
2020-12-13 16:02:09 +0100

    Add yellow buitoni

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import styled from "styled-components/macro";
 
+const theme = {
+	blue: {
+		default: "#3f51b5",
+		hover: "#283593",
+	},
+	pink: {
+		default: "#e91e63",
+		hover: "#ad1457",
+	},
+	yellow: {
+		default: "#f3de2c",
+		hover: "#fb6107",
+	},
+};
+
 const Button = styled.button`
-	background-color: #3949ab;
+	background-color: ${(props) => theme[props.theme].default};
 	color: white;
 	padding: 5px 15px;
 	border-radius: 5px;
 	outline: 0;
+	text-transform: uppercase;
+	margin: 10px 0px;
+	cursor: pointer;
 	box-shadow: 0px 5px 5px lightgray;
+	transition: ease background-color 500ms;
+	&:hover {
+		background-color: ${(props) => theme[props.theme].hover};
+	}
 `;
+
+Button.defaultProps = {
+	theme: "blue",
+};
 
 function clickMe() {
 	alert("You clicked me!");
@@ -15,9 +41,21 @@ function clickMe() {
 
 export default function App() {
 	return (
-		<div className="App">
-			<h2>Button-playground</h2>
-			<Button onClick={clickMe}>Button</Button>
-		</div>
+		<>
+			<div>
+				<h2>Button-playground</h2>
+				<Button onClick={clickMe}>Button</Button>
+			</div>
+			<div>
+				<Button theme="pink" onClick={clickMe}>
+					Button
+				</Button>
+			</div>
+			<div>
+				<Button theme="yellow" onClick={clickMe}>
+					Button
+				</Button>
+			</div>
+		</>
 	);
 }
